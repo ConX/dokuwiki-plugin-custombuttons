46f438795a5a90ab2186f948ebb234bd0f559b33
Xanthopoulos Constantinos <conx@xanthopoulos.info>
2010-07-21 02:57:25 +0300

    Fixes to genpng.php and action.php

--- genpng.php
+++ genpng.php
@@ -1,17 +1,15 @@
 <?php
-header("Content-type: image/png"); //Picture Format
-header("Expires: Mon, 01 Jul 2003 00:00:00 GMT"); // Past date
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // Consitnuously modified
-header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-header("Pragma: no-cache"); // NO CACHE
+header("Content-type: image/png");
+header("Expires: Mon, 01 Jul 2003 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
 
 /*image generation code*/
-//create Image of size 350px x 75px
-$bg = imagecreatetruecolor(isset($_GET['text']) ? strlen($_GET['text'])*11 : 66, 10);
+$bg = imagecreatetruecolor(isset($_GET['text']) ? strlen($_GET['text'])*11 : 66, 15);
 
 //This will make it transparent
 imagesavealpha($bg, true);
-
 $trans_colour = imagecolorallocatealpha($bg, 0, 0, 0, 127);
 imagefill($bg, 0, 0, $trans_colour);
 
