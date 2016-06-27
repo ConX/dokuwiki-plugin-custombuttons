2f4ea73cf6078d4eb873cdc36b3ecf189e27d0c9
Hideaki SAWADA <chuno@live.jp>
2016-06-27 13:15:06 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['name']                  = 'カスタムボタン設定';
+$lang['picker']                = 'カスタムボタン';
+$lang['txt_error']             = '設定ファイルに対する書込み権限がありません。';
+$lang['btnslist']              = 'ボタン一覧';
+$lang['btnslist_label']        = 'ラベル';
+$lang['btnslist_code']         = 'コード';
+$lang['btnslist_delete']       = '削除する？';
+$lang['btn_delete']            = '選択したボタンの削除';
+$lang['addbtn']                = 'ボタン追加';
+$lang['addbtn_icon']           = 'アイコン：';
+$lang['addbtn_textonly']       = '文字のみ';
+$lang['addbtn_label']          = 'ラベル：';
+$lang['addbtn_pretag']         = '前タグ：';
+$lang['addbtn_posttag']        = '後タグ：';
+$lang['addbtn_code']           = 'コード：';
+$lang['btn_add']               = '追加';
+$lang['txt_comment']           = '<b>*</b> 前後のタグなしで短縮操作ボタンを追加したい場合、入力欄を空のままにします。';
---
+++ lang/ja/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['usepicker']             = '追加されたカスタムボタン専用の摘みをツールバーに表示します。そうでない場合、カスタムボタンはツールバー上に追加されます。';
