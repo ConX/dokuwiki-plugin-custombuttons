afc5562c17c5b5888ae6ba0915f31cba7a4448bf
Sam01 <m.sajad079@gmail.com>
2016-08-24 16:40:27 +0200

    translation update

---
+++ lang/fa/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['name']                  = 'برپایی دکمه‌های سفارشی';
+$lang['picker']                = 'دکمه‌های سفارشی';
+$lang['txt_error']             = 'بدون دسترسی به فایل پیکربندی';
+$lang['btnslist']              = 'لیست دکمه‌ها';
+$lang['btnslist_label']        = 'برچسب';
+$lang['btnslist_code']         = 'کد';
+$lang['btnslist_delete']       = 'حذف؟';
+$lang['btn_delete']            = 'انتخاب شده حذف شد';
+$lang['addbtn']                = 'اضافه‌کردن دکمه';
+$lang['addbtn_icon']           = 'آیکون:';
+$lang['addbtn_textonly']       = 'فقط متن';
+$lang['addbtn_label']          = 'برچسب:';
+$lang['addbtn_pretag']         = 'برچسب قبل:';
+$lang['addbtn_posttag']        = 'برچسب ارسال:';
+$lang['addbtn_code']           = 'کد:';
+$lang['btn_add']               = 'اضافه‌کردن';
+$lang['txt_comment']           = '<b>*</b> اگر نمی‌خواهید یک دکمه‌ی میانبر با کد قبل و ارسال اضافه کنید این را خالی بگذارید';
---
+++ lang/fa/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['usepicker']             = 'استفاده از یک جمع کننده نوار ابزار برای دکمه های سفارشی اضافه شده، در غیر این صورت دکمه‌ها به‌طور مستقیم به نوارابزار اضافه می‌شود.';
