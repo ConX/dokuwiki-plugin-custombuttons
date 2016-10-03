d3f2bbccb0d8f75334a2d850824a09a23fe1334c
Eric <ericstevenart@netc.fr>
2016-10-03 16:35:05 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Eric <ericstevenart@netc.fr>
+ */
+$lang['name']                  = 'Installer Boutons Personnalisés';
+$lang['picker']                = 'Boutons Personnalisés';
+$lang['txt_error']             = 'Pas de droits d’écriture sur le fichier configuration';
+$lang['btnslist']              = 'Liste de boutons';
+$lang['btnslist_label']        = 'Étiquette';
+$lang['btnslist_code']         = 'Code';
+$lang['btnslist_delete']       = 'Effacer ?';
+$lang['btn_delete']            = 'Effacer sélecté';
+$lang['addbtn']                = 'Ajouter bouton';
+$lang['addbtn_icon']           = 'Icone :';
+$lang['addbtn_textonly']       = 'Texte seulement';
+$lang['addbtn_label']          = 'Étiquette :';
+$lang['addbtn_pretag']         = 'Pre-balise :';
+$lang['addbtn_posttag']        = 'Post-balise :';
+$lang['addbtn_code']           = 'Code :';
+$lang['btn_add']               = 'Ajouter';
+$lang['txt_comment']           = '<b>*</b> Si vous ne voulez pas ajouter un bouton raccourci avec pre et post-code , laisser ces champs vides.';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Eric <ericstevenart@netc.fr>
+ */
+$lang['usepicker']             = 'Utiliser un sélectionneur de barre à outils pour les boutons personalisés ajoutés, sinon les boutons sont directement ajoutés à la barre d’outils.';
