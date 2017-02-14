9a4afdeb49ca8ca3ef7caeddd3a7e6ca253cf584
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2017-02-14 07:00:39 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
+ */
+$lang['name']                  = 'Configuration des boutons personnalisés.';
+$lang['picker']                = 'Boutons personnalisés.';
+$lang['txt_error']             = 'Pas d\'accès en écriture au fichier de configuration.';
+$lang['btnslist']              = 'Liste des boutons';
+$lang['btnslist_label']        = 'Label';
+$lang['btnslist_code']         = 'Code';
+$lang['btnslist_delete']       = 'supprimer ?';
+$lang['btn_delete']            = 'suppression sélectionnée';
+$lang['addbtn']                = 'Ajouter le bouton';
+$lang['addbtn_icon']           = 'Icon:';
+$lang['addbtn_textonly']       = 'texte seul';
+$lang['addbtn_label']          = 'Label:';
+$lang['addbtn_pretag']         = 'préfixe:';
+$lang['addbtn_posttag']        = 'suffixe:';
+$lang['addbtn_code']           = 'Code:';
+$lang['btn_add']               = 'Ajouter';
+$lang['txt_comment']           = '<b>*</b> Si vous ne voulez pas ajouter un bouton de raccourci avec préfixe et suffixe, laisser ces champs vides.';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
+ */
+$lang['usepicker']             = 'Ajouter un sélecteur supplémentaire dans la barre d\'outils. Sinon, les boutons sont ajoutés directement dans la barre';
