b584c0483560f5c0e10cc73d01408de87f26c945
Schplurtz le Déboulonné <schplurtz@laposte.net>
2018-07-25 00:10:40 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['name']                  = 'Réglage des boutons personnalisés';
+$lang['picker']                = 'Boutons personnalisés';
+$lang['txt_error']             = 'Pas d\'accès en écriture au fichier de config';
+$lang['btnslist']              = 'Liste des boutons';
+$lang['btnslist_label']        = 'Label';
+$lang['btnslist_code']         = 'Code';
+$lang['btnslist_delete']       = 'Détruire ?';
+$lang['btn_delete']            = 'Détruire la sélection';
+$lang['addbtn']                = 'Ajouter un bouton';
+$lang['addbtn_icon']           = 'Icône :';
+$lang['addbtn_textonly']       = 'Texte seulement';
+$lang['addbtn_label']          = 'Label :';
+$lang['addbtn_code']           = 'Code :';
+$lang['btn_add']               = 'Ajout';
+$lang['txt_comment']           = '<b>*</b> Si vous ne voulez pas ajouter de bouton de raccourcis avec pre et post code, laissez ces champs vides.';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['usepicker']             = 'Utilisez un sélecteur de barre d\'outils pour les boutons personnalisés, sinon les boutons seront directement ajoutés à la barre d\'outils.';
