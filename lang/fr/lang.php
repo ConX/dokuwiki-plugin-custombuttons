da6da6bc27a97c06aa80b80334d0390dddf44e5d
Schplurtz le Déboulonné <schplurtz@laposte.net>
2018-10-26 03:40:05 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['name']                  = 'Réglage des boutons personnalisés';
+$lang['picker']                = 'Boutons personnalisés';
+$lang['txt_error']             = 'Pas d\'accès en écriture au fichier de config';
+$lang['btnslist']              = 'Liste de Boutons';
+$lang['btnslist_label']        = 'Label';
+$lang['btnslist_code']         = 'Code';
+$lang['btnslist_delete']       = 'Supprimer ?';
+$lang['btn_delete']            = 'Supprimer la sélection';
+$lang['addbtn']                = 'Ajouter un bouton';
+$lang['addbtn_icon']           = 'Icône';
+$lang['addbtn_textonly']       = 'Texte seul';
+$lang['addbtn_label']          = 'Label:';
+$lang['addbtn_pretag']         = 'Texte avant :';
+$lang['addbtn_posttag']        = 'Texte après :';
+$lang['addbtn_code']           = 'Code:';
+$lang['btn_add']               = 'Ajouter';
+$lang['txt_comment']           = 'b>*</b> Si vous ne voulez pas ajouter de bouton avec texte avant ou après, laisser ces champs vide';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['usepicker']             = 'Utiliser un sélecteur dans la barre d\'outils pour les boutons ajoutés; autrement les boutons sont ajouté directement dans la barre d\'outils.';
