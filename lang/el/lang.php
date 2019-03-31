76af0fbcce7b5230df141f7df47b7a26f4cb2c2d
Kakia Chatsiou <kakiac@gmail.com>
2019-03-31 18:30:27 +0200

    translation update

--- lang/el/lang.php
+++ lang/el/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Kakia Chatsiou <kakiac@gmail.com>
  * @author Consttantinos Xanthopoulos <conx@xanthopoulos.info>
  */
 $lang['name']                  = 'Εγκατάσταση του Custom Buttons';
@@ -14,11 +15,11 @@ $lang['btnslist_code']         = 'Κώδικας';
 $lang['btnslist_delete']       = 'Διαγραφή;';
 $lang['btn_delete']            = 'Διαγραφή επιλεγμένων';
 $lang['addbtn']                = 'Προσθήκη κουμπιού';
-$lang['addbtn_icon']           = 'Ικονίδιο';
+$lang['addbtn_icon']           = 'Εικονίδιο:';
 $lang['addbtn_textonly']       = 'μόνο κείμενο';
 $lang['addbtn_label']          = 'Επιγραφή:';
 $lang['addbtn_pretag']         = 'Πρόθεμα:';
 $lang['addbtn_posttag']        = 'Κατάληξη:';
-$lang['addbtn_code']           = 'Κώδικας';
+$lang['addbtn_code']           = 'Κώδικας:';
 $lang['btn_add']               = 'Προσθήκη';
-$lang['txt_comment']           = '<b>*</b> Εαν δεν θέλετε να προσθέσετε κουμπί συντόμευσης με πρόθμεα και κατάληξη αφήστε τα αντίστοιχα πεδία κενά.';
+$lang['txt_comment']           = '<b>*</b> Εαν δεν θέλετε να προσθέσετε κουμπί συντόμευσης με πρόθεμα και κατάληξη αφήστε τα αντίστοιχα πεδία κενά.';
