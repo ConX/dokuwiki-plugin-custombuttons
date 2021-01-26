396736b94f9c72fa863187d3ac663a649f323c0a
chtiland <fabrice@chtiland.com>
2021-01-26 19:03:03 +0100

    Update settings.php
    French language corrections.

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -3,6 +3,6 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author Eric <ericstevenart@netc.fr>
+ * @author Eric <ericstevenart@netc.fr>, 2021 :Fabrice Dejaigher <fabrice@chtiland.com>
  */
-$lang['usepicker']             = 'Utiliser un sélectionneur de barre à outils pour les boutons personalisés ajoutés, sinon les boutons sont directement ajoutés à la barre d’outils.';
+$lang['usepicker']             = 'Utiliser un bouton regroupant sur la barre d\'outils pour les boutons personalisés, sinon les boutons seront directement ajoutés  les uns derrières les autres.';
